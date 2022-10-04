37c8a6349783a4adafae67e665e6b4315b711f78
byunboy2 <76236947+byunboy2@users.noreply.github.com>
2022-10-04 16:24:50 -0700

    Created HTML template and added example template for the JS file

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
 console.log("Let's get this party started!");
+
+
+
+
+
+async function getGif() {
+  let gif = await axios.post(
+
+  );
+}
+
+
+
+
+$('#search').on("click", getGif);
+
+
+
+
+
+
+
+/* show result of borrowing in box */
+
+function showBorrow(res) {
+  $("#borrowed").html(res);
+}
+
+async function borrowMoney() {
+  let amount = Number($("#amount").val());
+
+  let response = await axios.post(
+    "/api/borrow", { amount });
+
+  console.log("borrow resp=", response);
+  showBorrow(response.data);
+}
+
+$("#borrow-btn").on("click", borrowMoney);
+``;
